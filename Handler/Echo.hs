ab85c6cb04543acb89a167dd02451801511556bf
Hiroyuki Kurokawa <pxi05647@nifty.com>
2014-03-02 15:15:26 +0900

    - update eto new version. Echo & Mirror are working fine but Blog is not yet.

--- Handler/Echo.hs
+++ Handler/Echo.hs
@@ -3,4 +3,5 @@ module Handler.Echo where
 import Import
 
 getEchoR :: Text -> Handler Html
+--getEchoR = error "Not yet implemented: getEchoR"
 getEchoR theText = defaultLayout $(widgetFile "echo")
